7e513c5f62cac709da31b21cbd28b79bff7b5c7e
Jacob Banks <kobybanks@gmail.com>
2021-10-13 15:02:20 -0400

    added typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,2 +1,47 @@
 // import the gql tagged template function
 const { gql } = require("apollo-server-express");
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBook: [book]
+  }
+
+  type Book {
+    bookid: ID
+    description: String
+    authors: String
+    title: String
+    image: Int
+    link: [Reaction]
+  }
+
+  input savedBook {
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+    authors: [String]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
